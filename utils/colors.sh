807947cc78f2a3d4c00911fd49f1cacee26ae220
enabeteleazar <nabet.eleazar@gmail.com>
2025-07-19 21:00:02 +0200

    ajout fonction install_neo > install.sh

--- utils/colors.sh
+++ utils/colors.sh
@@ -4,6 +4,7 @@ if [ "${NO_COLOR:-0}" -eq 0 ] && [ -t 1 ] && command -v tput >/dev/null 2>&1 && 
     RED='\033[0;31m'
     GREEN='\033[0;32m'
     BLUE='\033[0;34m'
+    CYAN='\033[0;36m'  
     YELLOW='\033[0;33m'
     NC='\033[0m' # No Color
 else
