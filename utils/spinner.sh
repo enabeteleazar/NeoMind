4a2d92dd734b5019897e2d3f79853d134baafda8
enabeteleazar <nabet.eleazar@gmail.com>
2025-07-19 20:20:37 +0200

    separation du code.

--- utils/spinner.sh
+++ utils/spinner.sh
@@ -1,14 +1,32 @@
+#!/bin/bash
 
-spinner() {
-    local pid=$1
-    local delay=0.1
-    local spinstr='|/-\'
-    while ps -p "$pid" > /dev/null 2>&1; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "${spinstr}"
-        spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
+spinner_pid=0
+
+start_spinner() {
+  local message="$1"
+  echo -ne "$message..."
+  spin &
+  spinner_pid=$!
+  disown
+}
+
+stop_spinner() {
+  local exit_code=$1
+  kill "$spinner_pid" > /dev/null 2>&1
+  wait "$spinner_pid" 2>/dev/null
+  if [[ $exit_code -eq 0 ]]; then
+    echo -e " ✅"
+  else
+    echo -e " ❌"
+  fi
+}
+
+spin() {
+  local -a spinners=('⠋' '⠙' '⠸' '⠴' '⠦' '⠇')
+  while true; do
+    for i in "${spinners[@]}"; do
+      echo -ne "\r$i"
+      sleep 0.1
     done
-    printf "      \b\b\b\b\b\b"
+  done
 }
