f2ea4f04bf8137e2cd660de602e1fddb8c02606f
enabeteleazar <nabet.eleazar@gmail.com>
2025-07-19 19:52:09 +0200

    creation dossier utils/Separation spinner.sh & colors.sh

---
+++ utils/spinner.sh
@@ -0,0 +1,14 @@
+
+spinner() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    while ps -p "$pid" > /dev/null 2>&1; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "${spinstr}"
+        spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "      \b\b\b\b\b\b"
+}
